c03734d30a3b0ff114625a4c05bce4ea6a560a45
Alexander Bowerman <xenwulf@gmail.com>
2024-11-21 19:22:00 -0500

    NinjaTools update

---
+++ mods/NinjaTools/NinjaTools/Items/ItemSmokeBomb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//  Vintage Story Includes
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+
+namespace NinjaTools.Items
+{
+    internal class ItemSmokeBomb : Item
+    {
+        public override void OnAttackingWith(IWorldAccessor world, Entity byEntity, Entity attackedEntity, ItemSlot itemslot)
+        {
+            //  Super
+            base.OnAttackingWith(world, byEntity, attackedEntity, itemslot);
+        }
+    }
+}
---
+++ mods/NinjaTools/NinjaTools/NinjaToolsModSystem.cs
@@ -0,0 +1,38 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+//  Mod Includes
+using NinjaTools.Items;
+
+namespace NinjaTools
+{
+    public class NinjaToolsModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            api.Logger.Notification("Hello from template mod: " + api.Side);
+
+            //  Register Item(s)
+            {
+                //  Smoke Bomb
+                api.RegisterItemClass(Mod.Info.ModID + ".smokebomb", typeof(ItemSmokeBomb));
+            }
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.Logger.Notification("Hello from template mod server side: " + Lang.Get("ninjatools:hello"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            api.Logger.Notification("Hello from template mod client side: " + Lang.Get("ninjatools:hello"));
+        }
+
+    }
+}
