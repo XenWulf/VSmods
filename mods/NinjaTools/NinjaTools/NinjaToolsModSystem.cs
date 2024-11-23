d53267ce9121581de0f041d268a114ed1097e464
Alexander Bowerman <xenwulf@gmail.com>
2024-11-23 16:52:44 -0500

    Edited NinjaTools to add Smokebomb

--- mods/NinjaTools/NinjaTools/NinjaToolsModSystem.cs
+++ mods/NinjaTools/NinjaTools/NinjaToolsModSystem.cs
@@ -3,7 +3,7 @@ using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
-//  Mod Includes
+//  NinjaTools Includes
 using NinjaTools.Items;
 
 namespace NinjaTools
@@ -15,23 +15,24 @@ namespace NinjaTools
         // Useful for registering block/entity classes on both sides
         public override void Start(ICoreAPI api)
         {
-            api.Logger.Notification("Hello from template mod: " + api.Side);
+            //api.Logger.Notification("Hello from template mod: " + api.Side);
 
             //  Register Item(s)
             {
+                //Mod.Info.ModID 
                 //  Smoke Bomb
-                api.RegisterItemClass(Mod.Info.ModID + ".smokebomb", typeof(ItemSmokeBomb));
+                api.RegisterItemClass(Mod.Info.ModID + ".smokebombitem", typeof(ItemSmokeBomb));
             }
         }
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-            api.Logger.Notification("Hello from template mod server side: " + Lang.Get("ninjatools:hello"));
+            //api.Logger.Notification("Hello from template mod server side: " + Lang.Get("ninjatools:hello"));
         }
 
         public override void StartClientSide(ICoreClientAPI api)
         {
-            api.Logger.Notification("Hello from template mod client side: " + Lang.Get("ninjatools:hello"));
+            //api.Logger.Notification("Hello from template mod client side: " + Lang.Get("ninjatools:hello"));
         }
 
     }
